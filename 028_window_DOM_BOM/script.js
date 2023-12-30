0cdd2c0fafd8468a8fa5ccdc69c23f6977c845fd
Akash Prajapati <akashprajapati1849@gmail.com>
2023-12-30 13:29:53 +0530

    028 window DOM BOM difference

---
+++ 028_window_DOM_BOM/script.js
@@ -0,0 +1,15 @@
+// window
+// everythings like console comes inside window
+window.console.log("Hello World!"); // generally we avoid writing window
+console.log(window);    // to see all window methods.
+
+// Document(DOM)
+console.log(document);
+console.log(document.body);
+function f1(color) {
+    document.body.style.background = color;
+}
+
+
+// BOM
+// location.href = "https://google.com"    // this will redirect to google.com

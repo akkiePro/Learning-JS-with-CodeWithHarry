f7797e817bf3050076737aa99a2c4d33e111b00f
Akash Prajapati <akashprajapati1849@gmail.com>
2024-01-02 17:32:00 +0530

    light/dark themes with className and classList

---
+++ 044_className_AND_classList/script.js
@@ -0,0 +1,6 @@
+// first.className = "text-black blue"
+// first.classList.add("blue")
+// first.classList.remove("blue")
+// first.classList.contains("arial")
+// first.classList.toggle("blue")
+// first.classList.toggle("dark-theme")

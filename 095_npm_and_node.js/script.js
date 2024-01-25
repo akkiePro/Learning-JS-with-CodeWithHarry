6a0f482c584cbe0ff19874ed6464adad1ac3ec67
Akash Prajapati <akashprajapati1849@gmail.com>
2024-01-25 17:03:02 +0530

    npm and node.js

---
+++ 095_npm_and_node.js/script.js
@@ -0,0 +1,13 @@
+/**
+ * following is a way to run .js file from terminal using node.js:
+ * nevigate to file => type following command
+ * => node .\filename.js
+ * 
+ * initi new npm(node package manager) project by following command:
+ * => npm init
+ * // for this, we need to provide details.
+ * => npm init -y
+ * // for this, no need to provide details.
+ * package.json will be created to respective folder.
+ */
+console.log("Hello using node.js");
